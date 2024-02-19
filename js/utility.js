31a906d157a84d6723cea0afb96d913b79e2bdfb
MushfikMahi <salehinmahi2004@gmail.com>
2024-02-19 20:38:22 +0600

    functionality seems to be done

---
+++ js/utility.js
@@ -0,0 +1,44 @@
+// function handleClick(event) {
+//     decreaseSeats();
+//     increaseSeat();
+//     event.target.removeEventListener('click', handleClick);
+// }
+
+// document.getElementById('a1').addEventListener('click', handleClick);
+// document.getElementById('a2').addEventListener('click', handleClick);
+// document.getElementById('a3').addEventListener('click', handleClick);
+// document.getElementById('a4').addEventListener('click', handleClick);
+// document.getElementById('b1').addEventListener('click', handleClick);
+// document.getElementById('b2').addEventListener('click', handleClick);
+// document.getElementById('b3').addEventListener('click', handleClick);
+// document.getElementById('b4').addEventListener('click', handleClick);
+
+    // element.target.removeEventListener('click', handleClick);
+
+
+    // if (selectedSeats >= 4) {
+    //     alert('You can only buy up to 4 seats at a time.');
+    //     return;
+    // }
+
+
+
+
+
+
+
+
+    // function applyForDiscount(){
+//     const coupon = document.getElementById('coupon-field')
+//     const couponText = coupon.value
+//     console.log(totalNum)
+//     console.log(couponText)
+//     let grand = 0
+//     if(couponText === 'couple20'){
+//         grand = totalNum*0.2
+//         // return grand
+//     }
+    
+//         console.log(typeof(grand))
+    
+// }
